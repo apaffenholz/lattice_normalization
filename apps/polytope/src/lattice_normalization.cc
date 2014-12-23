a1d33891bb096bf15ce847cdba21bbcec50a68fa
Andreas Paffenholz <paffenholz@mathematik.tu-darmstadt.de>
2014-12-23 15:08:20 +0100

    lex max ordering of a vector

--- apps/polytope/src/lattice_normalization.cc
+++ apps/polytope/src/lattice_normalization.cc
@@ -26,13 +26,53 @@ namespace polymake {
 
   namespace polytope {
 
+    // returns a permutation pi for the vector v such that pi(v) 
+    // is lex maixmal among all permutations of the entries of v
+    std::pair<Vector<Integer>, Vector<int> > FindMaxPermutation ( const Vector<Integer> & v ) {
+
+      const int dim = v.size();
+      Vector<Integer> base(v);
+      Vector<int> revperm(sequence(0,dim));
+
+      for ( int i = 1; i < dim; ++i ) {
+	int j = i;
+	Integer h = base[i];
+	int r = revperm[i];
+	while ( j > 0 && base[j-1] < h ) {
+	  base[j] = base[j-1];
+	  revperm[j] = revperm[j-1];
+	  j--;
+	}
+	base[j] = h;
+	revperm[j] =r;
+      }
+
+      Vector<int> perm(dim);
+      for ( int i = 0; i < dim; i++ ) {
+	perm[revperm[i]] = i;
+      }
+
+      std::pair<Vector<Integer>, Vector<int> > ret;
+      ret.first = base;
+      ret.second = perm;
+
+      return ret;
+    }
+
     
-    Matrix<Integer> lattice_normal_form ( const Matrix<Integer> & A ) {
+    Matrix<Integer> lattice_normal_form ( const perl::Object & p ) {
       
+      if ( ! p.give("LATTICE") ) 
+	throw std::runtime_error("the given polytope is not a lattice polytope");
+
+      Matrix<Integer> A = p.give("FACET_VERTEX_LATTICE_DISTANCES");
+
+      cout << FindMaxPermutation(A.row(0)) << endl;
+
       return common::HermiteNormalForm(A);
     } 
     
-    UserFunction4perl(" ", &lattice_normal_form, "lattice_normal_form($)");
+    UserFunction4perl(" ", &lattice_normal_form, "lattice_normal_form(Polytope)");
         
   }
 }
